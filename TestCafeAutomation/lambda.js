94a72a2a5b122ea40f7017256cc29ad050a53247
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-07-05 10:40:54 +0530

    remove the user pool and add a new

--- TestCafeAutomation/lambda.js
+++ TestCafeAutomation/lambda.js
@@ -1,15 +1,16 @@
 let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 exports.handler = function (event, context, callback) {
+
 	cognito_idp.adminCreateUser({
-		UserPoolId: process.env.UserPoolId_cognitonewUserPool, /* required */
-		Username: "andun@adroitlogic.com", /* required */
-		DesiredDeliveryMediums: ["SMS"],//[SMS | EMAIL,/* more items */],
+		UserPoolId: process.env.UserPoolId_cognitoTestUserPool, /* required */
+		Username: "AndunRanmal", /* required */
+		DesiredDeliveryMediums: ["EMAIL"],//[SMS | EMAIL,/* more items */],
 		ForceAliasCreation: false,
 		MessageAction: "RESEND",
 		TemporaryPassword: "Andun!12345",
-		UserAttributes: [],
-		ValidationData: []
+		UserAttributes: [{ Name: 'email', Value: 'andun@adroitlogic.com' }],
+		ValidationData: [{ Name: '', Value: '' }]
 	}, function (error, data) {
 		if (error) {
 			// implement error handling logic here
@@ -17,7 +18,6 @@ exports.handler = function (event, context, callback) {
 		}
 		// your logic goes within this block
 	});
-
 
 
 	callback(null, 'Successfully executed');
